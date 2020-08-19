25d094007750e96a3a82e19996a2c8291b53561d
19wh1a05a5 <69663979+19wh1a05a5@users.noreply.github.com>
2020-08-19 15:58:03 +0530

    Add files via upload

---
+++ week3/insertionsort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void insertion_sort(int a[100],int n)
+{
+	int i,j,index;
+	for(i=1;i<n;i++)
+	{
+		index=a[i];
+		j=i;
+		while((j>0) && (a[j-1]>index))
+		{
+			a[j]=a[j-1];
+			j=j-1;
+		}
+		a[j]=index;
+			
+	}
+}
+int main()
+{
+	int n,i,res;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	insertion_sort(a,n);
+	printf("Elements after sorting:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+}
+	
